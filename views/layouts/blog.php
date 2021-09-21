b3fe7e5d8554b51d60ead43e701b043b651c84bb
Bakalai <deveg98@gmail.com>
2021-09-21 19:03:34 +0700

    Перенос шаблона верстки в процессе, на видео 9:29

---
+++ views/layouts/blog.php
@@ -0,0 +1,191 @@
+<!DOCTYPE html>
+<html style="font-size: 16px;">
+  <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="utf-8">
+    <meta name="keywords" content="Блог, Разработчик, программирование">
+    <meta name="description" content="">
+    <meta name="page_type" content="np-template-header-footer-from-plugin">
+    <title>Главная страница сайта</title>
+    <link rel="stylesheet" href="nicepage.css" media="screen">
+<link rel="stylesheet" href="Главная.css" media="screen">
+    <script class="u-script" type="text/javascript" src="jquery.js" defer=""></script>
+    <script class="u-script" type="text/javascript" src="nicepage.js" defer=""></script>
+    <meta name="generator" content="Nicepage 3.25.0, nicepage.com">
+    
+    <link id="u-page-google-font" rel="stylesheet" href="https://fonts.googleapis.com/css?family=IBM+Plex+Mono:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i">
+    
+    
+    
+    <script type="application/ld+json">{
+		"@context": "http://schema.org",
+		"@type": "Organization",
+		"name": "",
+		"logo": "images/pngwing.com.png",
+		"sameAs": []
+}</script>
+    <meta name="theme-color" content="#478ac9">
+    <meta property="og:title" content="Главная страница сайта">
+    <meta property="og:type" content="website">
+  </head>
+  <body class="u-body"><header class="u-align-center-sm u-align-center-xs u-clearfix u-header u-section-row-container" id="sec-9d01"><div class="u-section-rows">
+        <div class="u-align-center-sm u-align-center-xs u-clearfix u-grey-25 u-section-row u-section-row-1" data-animation-name="" data-animation-duration="0" data-animation-delay="0" data-animation-direction="" id="sec-09b1">
+          <div class="u-clearfix u-sheet u-sheet-1">
+            <a href="https://nicepage.com" class="u-image u-logo u-image-1" data-image-width="1600" data-image-height="1321">
+              <img src="images/pngwing.com.png" class="u-logo-image u-logo-image-1">
+            </a>
+            <div class="u-social-icons u-spacing-10 u-social-icons-1">
+              <a class="u-social-url" target="_blank" title="Telegram" href=""><span class="u-icon u-social-icon u-social-telegram u-icon-1"><svg class="u-svg-link" preserveAspectRatio="xMidYMin slice" viewBox="0 0 112 112" style=""><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#svg-cb44"></use></svg><svg class="u-svg-content" viewBox="0 0 112 112" x="0" y="0" id="svg-cb44"><circle fill="currentColor" cx="56.1" cy="56.1" r="55"></circle><path fill="#FFFFFF" d="M18.4,53.2l64.7-24.9c3-1.1,5.6,0.7,4.7,5.3l0,0l-11,51.8c-0.8,3.7-3,4.6-6.1,2.8L53.9,75.8l-8.1,7.8
+	c-0.9,0.9-1.7,1.6-3.4,1.6l1.2-17l31.1-28c1.4-1.2-0.3-1.9-2.1-0.7L34.2,63.7l-16.6-5.2C14,57.4,14,54.9,18.4,53.2L18.4,53.2z"></path></svg></span>
+              </a>
+              <a class="u-social-url" target="_blank" title="Email" href=""><span class="u-icon u-social-email u-social-icon u-icon-2"><svg class="u-svg-link" preserveAspectRatio="xMidYMin slice" viewBox="0 0 112 112" style=""><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#svg-8d09"></use></svg><svg class="u-svg-content" viewBox="0 0 112 112" x="0" y="0" id="svg-8d09"><circle fill="currentColor" cx="56.1" cy="56.1" r="55"></circle><path id="path3864" fill="#FFFFFF" d="M27.2,28h57.6c4,0,7.2,3.2,7.2,7.2l0,0v42.7c0,3.9-3.2,7.2-7.2,7.2l0,0H27.2
+	c-4,0-7.2-3.2-7.2-7.2V35.2C20,31.1,23.2,28,27.2,28 M56,52.9l28.8-17.8H27.2L56,52.9 M27.2,77.7h57.6V43.5L56,61.3L27.2,43.5V77.7z
+	"></path></svg></span>
+              </a>
+            </div>
+            <nav class="u-align-left u-menu u-menu-dropdown u-offcanvas u-menu-1">
+              <div class="menu-collapse" style="font-size: 1rem;">
+                <a class="u-button-style u-nav-link" href="#">
+                  <svg class="u-svg-link" preserveAspectRatio="xMidYMin slice" viewBox="0 0 302 302" style=""><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#svg-8a8f"></use></svg>
+                  <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" id="svg-8a8f" x="0px" y="0px" viewBox="0 0 302 302" style="enable-background:new 0 0 302 302;" xml:space="preserve" class="u-svg-content"><g><rect y="36" width="302" height="30"></rect><rect y="236" width="302" height="30"></rect><rect y="136" width="302" height="30"></rect>
+</g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g></svg>
+                </a>
+              </div>
+              <div class="u-custom-menu u-nav-container">
+                <ul class="u-nav u-unstyled u-nav-1"><li class="u-nav-item"><a class="u-button-style u-nav-link u-text-active-palette-1-base u-text-hover-palette-2-base" href="Главная.html" style="padding: 10px 30px;">Главная</a>
+</li><li class="u-nav-item"><a class="u-button-style u-nav-link u-text-active-palette-1-base u-text-hover-palette-2-base" href="Проекты.html" style="padding: 10px 30px;">Проекты</a>
+</li><li class="u-nav-item"><a class="u-button-style u-nav-link u-text-active-palette-1-base u-text-hover-palette-2-base" href="О-проекте.html" style="padding: 10px 30px;">О проекте</a>
+</li></ul>
+              </div>
+              <div class="u-custom-menu u-nav-container-collapse">
+                <div class="u-align-center u-black u-container-style u-inner-container-layout u-opacity u-opacity-95 u-sidenav">
+                  <div class="u-sidenav-overflow">
+                    <div class="u-menu-close"></div>
+                    <ul class="u-align-center u-nav u-popupmenu-items u-unstyled u-nav-2"><li class="u-nav-item"><a class="u-button-style u-nav-link" href="Главная.html" style="padding: 10px 42px;">Главная</a>
+</li><li class="u-nav-item"><a class="u-button-style u-nav-link" href="Проекты.html" style="padding: 10px 42px;">Проекты</a>
+</li><li class="u-nav-item"><a class="u-button-style u-nav-link" href="О-проекте.html" style="padding: 10px 42px;">О проекте</a>
+</li></ul>
+                  </div>
+                </div>
+                <div class="u-black u-menu-overlay u-opacity u-opacity-70"></div>
+              </div>
+            </nav>
+            <h3 class="u-custom-font u-headline u-text u-text-1">
+              <a href="/">Путь самурая</a>
+            </h3>
+          </div>
+          
+          
+          
+          
+          
+        </div>
+        <div class="u-custom-color-4 u-section-row u-section-row-2" id="sec-d6e2">
+          <div class="u-clearfix u-sheet u-valign-middle u-sheet-2">
+            <form action="#" method="get" class="u-border-1 u-border-grey-30 u-search u-search-left u-white u-search-1">
+              <button class="u-search-button" type="submit">
+                <span class="u-search-icon u-spacing-10">
+                  <svg class="u-svg-link" preserveAspectRatio="xMidYMin slice" viewBox="0 0 56.966 56.966"><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#svg-55be"></use></svg>
+                  <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" id="svg-55be" x="0px" y="0px" viewBox="0 0 56.966 56.966" style="enable-background:new 0 0 56.966 56.966;" xml:space="preserve" class="u-svg-content"><path d="M55.146,51.887L41.588,37.786c3.486-4.144,5.396-9.358,5.396-14.786c0-12.682-10.318-23-23-23s-23,10.318-23,23  s10.318,23,23,23c4.761,0,9.298-1.436,13.177-4.162l13.661,14.208c0.571,0.593,1.339,0.92,2.162,0.92  c0.779,0,1.518-0.297,2.079-0.837C56.255,54.982,56.293,53.08,55.146,51.887z M23.984,6c9.374,0,17,7.626,17,17s-7.626,17-17,17  s-17-7.626-17-17S14.61,6,23.984,6z"></path></svg>
+                </span>
+              </button>
+              <input class="u-search-input" type="search" name="search" value="" placeholder="Search">
+            </form>
+          </div>
+          
+          
+          
+          
+          
+        </div>
+      </div></header>
+    <section class="u-align-left u-clearfix u-image u-shading u-section-1" src="" data-image-width="1280" data-image-height="850" id="sec-f8d2">
+      <div class="u-clearfix u-sheet u-sheet-1">
+        <h1 class="u-text u-text-default u-title u-text-1">блог разработчика </h1>
+        <p class="u-large-text u-text u-text-variant u-text-2">Основной проект, в который будут добавляться дальнейшие наработки</p>
+        <a href="#" class="u-btn u-button-style u-palette-2-base u-btn-1">узнать больше</a>
+      </div>
+    </section>
+    <section class="u-align-center u-clearfix u-section-2" id="carousel_6d25">
+      <div class="u-clearfix u-sheet u-sheet-1"><!--blog--><!--blog_options_json--><!--{"type":"Recent","source":"","tags":"","count":""}--><!--/blog_options_json-->
+        <div class="u-blog u-blog-1">
+          <div class="u-repeater u-repeater-1"><!--blog_post-->
+            <div class="u-blog-post u-container-style u-repeater-item">
+              <div class="u-container-layout u-similar-container u-valign-bottom-xs u-container-layout-1">
+                <a class="u-post-header-link" href="blog/пост.html"><!--blog_post_image-->
+                  <img src="images/2.jpeg" alt="" class="u-blog-control u-image u-image-default u-image-1"><!--/blog_post_image-->
+                </a><!--blog_post_header-->
+                <h2 class="u-blog-control u-text u-text-1">
+                  <a class="u-post-header-link" href="blog/пост.html"><!--blog_post_header_content-->Заголовок 1-го поста<!--/blog_post_header_content--></a>
+                </h2><!--/blog_post_header--><!--blog_post_content-->
+                <div class="u-blog-control u-post-content u-text u-text-2"><!--blog_post_content_content-->Пример короткого текста. Lorem ipsum dolor sit amet.<!--/blog_post_content_content--></div><!--/blog_post_content--><!--blog_post_metadata-->
+                <div class="u-blog-control u-metadata u-metadata-1"><!--blog_post_metadata_date-->
+                  <span class="u-meta-date u-meta-icon"><!--blog_post_metadata_date_content-->Tue Sep 21 2021<!--/blog_post_metadata_date_content--></span><!--/blog_post_metadata_date-->
+                </div><!--/blog_post_metadata--><!--blog_post_readmore-->
+                <a href="blog/пост.html" class="u-blog-control u-border-2 u-border-grey-dark-1 u-btn u-btn-rectangle u-button-style u-none u-btn-1"><!--blog_post_readmore_content--><!--options_json--><!--{"content":"Read More"}--><!--/options_json-->Read More<!--/blog_post_readmore_content--></a><!--/blog_post_readmore-->
+              </div>
+            </div><!--/blog_post--><!--blog_post-->
+            <div class="u-blog-post u-container-style u-repeater-item">
+              <div class="u-container-layout u-similar-container u-valign-bottom-xs u-container-layout-2">
+                <a class="u-post-header-link" href="blog/пост-1.html"><!--blog_post_image-->
+                  <img src="images/3.jpeg" alt="" class="u-blog-control u-image u-image-default u-image-2"><!--/blog_post_image-->
+                </a><!--blog_post_header-->
+                <h2 class="u-blog-control u-text u-text-3">
+                  <a class="u-post-header-link" href="blog/пост-1.html"><!--blog_post_header_content-->Заголовок 2-го поста<!--/blog_post_header_content--></a>
+                </h2><!--/blog_post_header--><!--blog_post_content-->
+                <div class="u-blog-control u-post-content u-text u-text-4"><!--blog_post_content_content-->Пример короткого текста. Lorem ipsum dolor sit amet.<!--/blog_post_content_content--></div><!--/blog_post_content--><!--blog_post_metadata-->
+                <div class="u-blog-control u-metadata u-metadata-2"><!--blog_post_metadata_date-->
+                  <span class="u-meta-date u-meta-icon"><!--blog_post_metadata_date_content-->Tue Sep 21 2021<!--/blog_post_metadata_date_content--></span><!--/blog_post_metadata_date-->
+                </div><!--/blog_post_metadata--><!--blog_post_readmore-->
+                <a href="blog/пост-1.html" class="u-blog-control u-border-2 u-border-grey-dark-1 u-btn u-btn-rectangle u-button-style u-none u-btn-2"><!--blog_post_readmore_content--><!--options_json--><!--{"content":"Read More"}--><!--/options_json-->Read More<!--/blog_post_readmore_content--></a><!--/blog_post_readmore-->
+              </div>
+            </div><!--/blog_post--><!--blog_post-->
+            <div class="u-blog-post u-container-style u-repeater-item">
+              <div class="u-container-layout u-similar-container u-valign-bottom-xs u-container-layout-3">
+                <a class="u-post-header-link" href="blog/пост-2.html"><!--blog_post_image-->
+                  <img src="images/4.jpeg" alt="" class="u-blog-control u-image u-image-default u-image-3"><!--/blog_post_image-->
+                </a><!--blog_post_header-->
+                <h2 class="u-blog-control u-text u-text-5">
+                  <a class="u-post-header-link" href="blog/пост-2.html"><!--blog_post_header_content-->Заголовок 3-го поста<!--/blog_post_header_content--></a>
+                </h2><!--/blog_post_header--><!--blog_post_content-->
+                <div class="u-blog-control u-post-content u-text u-text-6"><!--blog_post_content_content-->Пример короткого текста. Lorem ipsum dolor sit amet.<!--/blog_post_content_content--></div><!--/blog_post_content--><!--blog_post_metadata-->
+                <div class="u-blog-control u-metadata u-metadata-3"><!--blog_post_metadata_date-->
+                  <span class="u-meta-date u-meta-icon"><!--blog_post_metadata_date_content-->Tue Sep 21 2021<!--/blog_post_metadata_date_content--></span><!--/blog_post_metadata_date-->
+                </div><!--/blog_post_metadata--><!--blog_post_readmore-->
+                <a href="blog/пост-2.html" class="u-blog-control u-border-2 u-border-grey-dark-1 u-btn u-btn-rectangle u-button-style u-none u-btn-3"><!--blog_post_readmore_content--><!--options_json--><!--{"content":"Read More"}--><!--/options_json-->Read More<!--/blog_post_readmore_content--></a><!--/blog_post_readmore-->
+              </div>
+            </div><!--/blog_post-->
+          </div>
+        </div><!--/blog-->
+      </div>
+    </section>
+    
+    
+    <footer class="u-align-center-md u-align-center-sm u-align-center-xs u-clearfix u-footer u-grey-80" id="sec-e31e"><div class="u-clearfix u-sheet u-sheet-1">
+        <a href="https://nicepage.com" class="u-image u-logo u-image-1" data-image-width="1600" data-image-height="1321">
+          <img src="images/pngwing.com.png" class="u-logo-image u-logo-image-1">
+        </a>
+        <div class="u-social-icons u-spacing-10 u-social-icons-1">
+          <a class="u-social-url" target="_blank" title="Telegram" href=""><span class="u-icon u-social-icon u-social-telegram u-icon-1"><svg class="u-svg-link" preserveAspectRatio="xMidYMin slice" viewBox="0 0 112 112" style=""><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#svg-1d23"></use></svg><svg class="u-svg-content" viewBox="0 0 112 112" x="0" y="0" id="svg-1d23"><circle fill="currentColor" cx="56.1" cy="56.1" r="55"></circle><path fill="#FFFFFF" d="M18.4,53.2l64.7-24.9c3-1.1,5.6,0.7,4.7,5.3l0,0l-11,51.8c-0.8,3.7-3,4.6-6.1,2.8L53.9,75.8l-8.1,7.8
+	c-0.9,0.9-1.7,1.6-3.4,1.6l1.2-17l31.1-28c1.4-1.2-0.3-1.9-2.1-0.7L34.2,63.7l-16.6-5.2C14,57.4,14,54.9,18.4,53.2L18.4,53.2z"></path></svg></span>
+          </a>
+          <a class="u-social-url" target="_blank" title="Email" href=""><span class="u-icon u-social-email u-social-icon u-icon-2"><svg class="u-svg-link" preserveAspectRatio="xMidYMin slice" viewBox="0 0 112 112" style=""><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#svg-bb1b"></use></svg><svg class="u-svg-content" viewBox="0 0 112 112" x="0" y="0" id="svg-bb1b"><circle fill="currentColor" cx="56.1" cy="56.1" r="55"></circle><path id="path3864" fill="#FFFFFF" d="M27.2,28h57.6c4,0,7.2,3.2,7.2,7.2l0,0v42.7c0,3.9-3.2,7.2-7.2,7.2l0,0H27.2
+	c-4,0-7.2-3.2-7.2-7.2V35.2C20,31.1,23.2,28,27.2,28 M56,52.9l28.8-17.8H27.2L56,52.9 M27.2,77.7h57.6V43.5L56,61.3L27.2,43.5V77.7z
+	"></path></svg></span>
+          </a>
+        </div>
+        <p class="u-text u-text-1">Подвал сайта, сюда я что-нибудь да добавлю...</p>
+      </div></footer>
+    <section class="u-backlink u-clearfix u-grey-80">
+      <a class="u-link" href="https://nicepage.com/website-templates" target="_blank">
+        <span>Website Templates</span>
+      </a>
+      <p class="u-text">
+        <span>created with</span>
+      </p>
+      <a class="u-link" href="https://nicepage.com/" target="_blank">
+        <span>Website Builder Software</span>
+      </a>. 
+    </section>
+  </body>
+</html>
