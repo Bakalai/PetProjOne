4602fb92dbdb330e81818a978698d7a016f2e773
Bakalai <deveg98@gmail.com>
2021-09-29 14:24:14 +0700

    Перенёс наконец-то шаблон, теперь будем работать с MVС

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -14,7 +14,7 @@ AppAsset::register($this);
 ?>
 <?php $this->beginPage() ?>
 <!DOCTYPE html>
-<html lang="<?= Yii::$app->language ?>" class="h-100">
+<html class="h-100"  lang="<? Yii::$app->language ?>" >
 <head>
     <meta charset="<?= Yii::$app->charset ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
